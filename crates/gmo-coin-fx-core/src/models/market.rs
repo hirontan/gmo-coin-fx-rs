0d9be3ed90a90ffbf247e5f4d9ec7e5aed3d2ae0
hirontan <hironori.t.0405@gmail.com>
2026-05-01 14:34:19 +0900

    feat: APIから返却されるレスポンスモデル（ApiStatus, Ticker, Kline, Symbol）を定義

--- crates/gmo-coin-fx-core/src/models/market.rs
+++ crates/gmo-coin-fx-core/src/models/market.rs
@@ -8,3 +8,31 @@ pub struct Ticker {
     pub timestamp: String,
     pub status: String,
 }
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ApiStatus {
+    pub status: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Kline {
+    #[serde(rename = "openTime")]
+    pub open_time: String,
+    pub open: String,
+    pub high: String,
+    pub low: String,
+    pub close: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Symbol {
+    pub symbol: String,
+    #[serde(rename = "minOpenOrderSize")]
+    pub min_open_order_size: String,
+    #[serde(rename = "maxOrderSize")]
+    pub max_order_size: String,
+    #[serde(rename = "sizeStep")]
+    pub size_step: String,
+    #[serde(rename = "tickSize")]
+    pub tick_size: String,
+}
